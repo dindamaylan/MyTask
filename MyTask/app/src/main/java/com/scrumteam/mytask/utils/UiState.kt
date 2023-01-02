4fb221340a545cff96c040fc0a4ec9399a3e8e8c
rezachyno <62907085+rezacahyono@users.noreply.github.com>
2023-01-03 01:56:35 +0700

    Feat: adding profile, change password, logout bottom sheet (#3)
    * Feat: Authentication, but bug 1 register
    
    * Feat: adding profile, change password, logout bottom sheet

--- MyTask/app/src/main/java/com/scrumteam/mytask/utils/UiState.kt
+++
@@ -1,8 +0,0 @@
-package com.scrumteam.mytask.utils
-
-data class UiState (
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-    val isSuccess: Boolean = false,
-    val message: UiText? = null,
-)
